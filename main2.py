94488ac909077fe47ea8c1b4f3639ef555ae28d3
Shashwat <20je0898@github.com>
2025-05-09 15:14:59 +0530

    modified main2.py

--- main2.py
+++ main2.py
@@ -22,27 +22,27 @@ trj.reset()
 # epochs = 50
 
 # trainer.run(epochs, trj, actor, critic, actor_optimizer, critic_optimizer)
-for year in tqdm(range(2020,1895,-5)):
-    pred,true = evaluate.run(year)[3:5]
-    pred = pred.squeeze(0)
+# for year in tqdm(range(2020,1895,-5)):
+pred,true = evaluate.run(1995)[3:5]
+pred = pred.squeeze(0)
 
-    plt.plot(torch.log(abs(pred)), label='pred')
-    plt.plot(torch.log(abs(true)), label='true')
-    plt.xlabel('Coefficient id')
-    plt.ylabel('abs(Value)')
-    plt.title(f'Year {year}')
-    plt.legend()
-    plt.savefig(f'Plots/coeff_plot_{year}.png', dpi=600)
-    plt.clf()
+#     plt.plot(torch.log(abs(pred)), label='pred')
+#     plt.plot(torch.log(abs(true)), label='true')
+#     plt.xlabel('Coefficient id')
+#     plt.ylabel('abs(Value)')
+#     plt.title(f'Year {year}')
+#     plt.legend()
+#     plt.savefig(f'Plots/coeff_plot_{year}.png', dpi=600)
+#     plt.clf()
 
     # plots.contour_plot(trj, pred, year)
 
-#     LON, LAT, (F, B_r, B_theta, B_phi) = trj.contour_plot(1000,1000,true)
-#     div1 = trj.divergence(6371.2, 90-LAT, LON, B_r, B_theta, B_phi)
+LON, LAT, (F, B_r, B_theta, B_phi) = trj.contour_plot(1000,1000,true)
+div1 = trj.divergence(6371.2, 90-LAT, LON, B_r, B_theta, B_phi)
 
-#     LON, LAT, (F, B_r, B_theta, B_phi) = trj.contour_plot(1000,1000,pred)
-#     div2 = trj.divergence(6371.2, 90-LAT, LON, B_r, B_theta, B_phi)
-#     print(div1>div2)
+LON, LAT, (F, B_r, B_theta, B_phi) = trj.contour_plot(1000,1000,pred)
+div2 = trj.divergence(6371.2, 90-LAT, LON, B_r, B_theta, B_phi)
+print(100/div1, 100/div2)
 
 # evaluate.run(1945)
 # year = 1920
